1dfd01e0b0eaa2591748adc8b57dcafa4f5f85c4
marszaleklukasz <50378595+marszaleklukasz@users.noreply.github.com>
2019-06-28 17:53:02 +0200

    Update EntityApiFactory.php
    add "createOauth" function

--- src/Entity/EntityApiFactory.php
+++ src/Entity/EntityApiFactory.php
@@ -3,6 +3,7 @@
 namespace Webit\WFirmaSDK\Entity;
 
 use Webit\WFirmaSDK\Auth\BasicAuth;
+use Webit\WFirmaSDK\Auth\OauthAuth;
 use Webit\WFirmaSDK\Entity\Infrastructure\BasicAuthBasedRequestExecutorFactory;
 use Webit\WFirmaSDK\Entity\Infrastructure\Buzz\BuzzRequestExecutorFactory;
 
@@ -24,6 +25,17 @@ class EntityApiFactory
     {
         return new DefaultEntityApi(
             $this->executorFactory->create($auth)
+        );
+    }
+    
+    /**
+     * @param OauthAuth $auth
+     * @return DefaultEntityApi
+     */
+    public function createOauth(OauthAuth $auth)
+    {
+        return new DefaultEntityApi(
+            $this->executorFactory->createOauth($auth)
         );
     }
 }
